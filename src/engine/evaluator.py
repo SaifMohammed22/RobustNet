f51c48dd098953497882b9542e31e733b5b107b7
SaifMohammed22 <ssaifmohammed04@gmail.com>
2026-08-01 20:15:16 +0300

    Add a notebook for colab training, remove logger from engines

--- src/engine/evaluator.py
+++ src/engine/evaluator.py
@@ -7,7 +7,6 @@ logger = get_logger(__name__)
 
 def test_one_epoch(cfg, model, test_loader, loss_fn):
     running_loss = 0.0
-    last_loss = 0.0
 
     with torch.no_grad():
         for i, data in enumerate(test_loader):
@@ -18,8 +17,5 @@ def test_one_epoch(cfg, model, test_loader, loss_fn):
             outputs = model(images)
             loss = loss_fn(outputs, labels)
             running_loss += loss.item()
-            if (i + 1) % cfg.SOLVER.LOG_PERIOD == 0:
-                last_loss = running_loss / cfg.SOLVER.LOG_PERIOD
-                # print(f"    batch {i + 1} test loss: {last_loss}")
-                running_loss = 0.0
-    return last_loss
+
+    return running_loss
--- src/engine/trainer.py
+++ src/engine/trainer.py
@@ -6,7 +6,6 @@ logger = get_logger(__name__)
 
 def train_one_epoch(cfg, model, optimizer, train_loader, loss_fn):
     running_loss = 0.0
-    last_loss = 0.0
 
     for i, data in enumerate(train_loader):
         images, labels = data
@@ -20,8 +19,5 @@ def train_one_epoch(cfg, model, optimizer, train_loader, loss_fn):
         optimizer.step()
 
         running_loss += loss.item()
-        if (i + 1) % cfg.SOLVER.LOG_PERIOD == 0:
-            last_loss = running_loss / cfg.SOLVER.LOG_PERIOD
-            # logger.info(f"  batch {i + 1} train loss: {last_loss}")
-            running_loss = 0.0
-    return last_loss
+
+    return running_loss
