35db238e27cd13260935304e830a5f1fbb19c666
SaifMohammed22 <ssaifmohammed04@gmail.com>
2026-08-31 22:47:13 +0300

    Fix weight_decay variable duplication

--- src/utils/build_optim.py
+++ src/utils/build_optim.py
@@ -10,7 +10,7 @@ def build_optimizer(cfg, model):
         weight_decay = cfg.SOLVER.WEIGHT_DECAY
         if "bias" in key:
             lr = cfg.SOLVER.BASE_LR * cfg.SOLVER.BIAS_LR_FACTOR
-            weight_decay = weight_decay = cfg.SOLVER.WEIGHT_DECAY_BIAS
+            weight_decay = cfg.SOLVER.WEIGHT_DECAY_BIAS
         params += [{"params": [val], "lr": lr, "weight_decay": weight_decay}]
     optimizer = getattr(torch.optim, cfg.SOLVER.OPTIMIZER_NAME)(
         params, momentum=cfg.SOLVER.MOMENTUM)
